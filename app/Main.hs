c7d2b594fae579e8fe6baa972385daec920655bb
Mikaël Dusenne <mikaeldusenne@gmail.com>
2020-02-27 06:38:25 -0500

    not unified

--- app/Main.hs
+++ app/Main.hs
@@ -15,29 +15,35 @@ import Misc
 import Tuple
 import System.Environment (getEnv, getArgs)
 import Control.Monad (when, foldM_)
+import Control.Concurrent (threadDelay)
 
+type Pathl = [String]
+type T = (Pathl, String)
+type T' = IO [(String, String)]
 
 base = "https://www.persianpod101.com/"
 
 mkurl s = base </> applyIf ((== '/').head) tail s
 
-l = words "absolute-beginner beginner intermediate advanced bonus"
+l = reverse $ words "absolute-beginner beginner intermediate advanced bonus"
 
 cookiefilepath = "cookies"
 
 login [login, pass] = curlPost "https://www.persianpod101.com/member/login_new.php" ["amember_login="++login,"amember_pass="++pass]
 
+login' = mapM (getEnv . ("pp101_"++)) ["LOGIN", "PASS"] >>= login
+
 openURL x = snd <$> curlGetString x [CurlFollowLocation True, CurlCookieFile cookiefilepath]
 
 isalreadythere dest = (any (== basename dest) . map dropExtension ) <$> listDirectory (dirname dest)
 
-download url dest = openURIWithOpts [CurlFollowLocation True, CurlCookieFile cookiefilepath] url
+download url dest = threadDelay (2*10^5) >> openURIWithOpts [CurlFollowLocation True, CurlCookieFile cookiefilepath] url
                     >>= dltmp
   where dltmp (Right bs) = BS.writeFile tmppath bs >> renamePath tmppath dest
-        dltmp (Left err) = print err
+        dltmp (Left err) = putStrLn err
         tmppath = "tmp.persianpod101.download"
 
-extracturl url f = (f . parseTags) <$> openURL url
+extracturl url f = threadDelay (10^5) >> (f . parseTags) <$> openURL url
 
 trimm = reverse . f . reverse . f
   where f = dropWhile (`elem` "\r\n \t?")
@@ -45,41 +51,26 @@ trimm = reverse . f . reverse . f
 mkname e = map f . trimm $ e
   where f x = applyIf (not.(`elem` alpha_num)) (\_ -> '_') x
 
--- mkdest l = foldl (</>) "" . (l++) . (:[])
+href = (mkurl . fromAttrib "href")
 
-hreff = (mkurl . fromAttrib "href")
+-- tag_class_f t cl f = map f . filter (~== TagOpen t [("class", cl)])
+tag_class_f t cl f = map f . filter ((cl `elem`) . words . fromAttrib "class") . filter (~== TagOpen t [])
 
-tag_class_f t cl f = map f . filter (~== TagOpen t [("class", cl)])
-a_class_href cl = tag_class_f "a" cl hreff
+a_class_href cl = tag_class_f "a" cl href
 
--- goo download_dest section (k, (name, url3)) = do
---   let dest = mkdest [download_dest, section] $ mkname k name
---   createDirectoryIfMissing True $ dirname dest
-  
---   isalreadythere dest >>=
---     ((putStrLn $ "skipping '"++dest++"', file exists.") !∫
---      (do
---          putStrLn $ "Dl " ++ url3 ++ "..."
---          url4 <- parseTags <$> openURL url3
---          let fs = map (safe_head.) [
---                tag_class_f "button" "js-lsn3-play-lesson-audio" (fromAttrib "data-url"),
---                tag_class_f "video" "js-lsn3-main-video-player" (fromAttrib "data-trackurl")
---                ]
---              process Nothing = return "no"
---              process (Just e) = download e (dest++takeExtension e) >> return "ok"
---          putStrLn $ show k ++ " --> " ++ dest
---          ans <- mapM (process . ($url4) ) fs
---          when (all (=="no") ans) (putStrLn $ "error with " ++ url3)
---      ))
-    
+textIn tag match = map (fromTagText  . head) . tail . splitOn  (~== TagOpen tag match)
+extract_lesson_name = textIn "div" [("class", "cl-lesson__title")]
+
 goo dldir p url3 = do
   let dest =  reduce (</>) $ dldir:p
+  putStr dest
   createDirectoryIfMissing True $ dirname dest
   
   isalreadythere dest >>=
-    ((putStrLn $ "skipping '"++dest++"', file exists.") !∫
+    ((putStrLn $ " skipping, file exists.") !∫
      (do
-         putStrLn $ "Dl " ++ url3 ++ "..."
+         print "Download..."
+         putStrLn $ nice'title url3
          url4 <- parseTags <$> openURL url3
          let fs = map (safe_head.) [
                tag_class_f "button" "js-lsn3-play-lesson-audio" (fromAttrib "data-url"),
@@ -87,63 +78,47 @@ goo dldir p url3 = do
                ]
              process Nothing = return "no"
              process (Just e) = download e (dest++takeExtension e) >> return "ok"
-         putStrLn dest
          ans <- mapM (process . ($url4) ) fs
-         when (all (=="no") ans) (putStrLn $ "error with " ++ url3)
+         when (all (=="no") ans) (
+           do
+             print url4
+             putStrLn $ "error with " ++ url3
+             when ((>0) $ length . filter (~== TagOpen "button" [("aria-label","Please sign in")]) $ url4) (
+               do
+                 putStrLn "Re-logging-in..."
+                 login'
+               )
+           )
      ))
 
-extract_lesson_name = map (fromTagText  . head) . tail . splitOn  (~== TagOpen "div" [("class", "cl-lesson__title")])
-
-f :: [T] -> [T -> T'] -> IO ()
 f u (fm:fms) = do
   (`mapM_` u) (\(a, b) -> do
-            print a
-            ans <- fm (a, b)
-            f (map (applyToFst (($a) . append)) ans) fms
-        )
+                  -- print a
+                  ans <- fm (a, b)
+                  f (map (applyToFst (($a) . append)) ans) fms
+              )
 f u [] = return ()
 
-type Pathl = [String]
-type T = (Pathl, String)
-type T' = IO [(String, String)]
 
+a_href_f f cl (_, b) =  b `extracturl` (\tags -> let hr = (a_class_href cl tags)
+                                                 in zip (map f hr) hr)
+
+a_href_basename = do
+  a_href_f basename
+prepare_lesson = \(a,b) -> b `extracturl` (\tags -> zip
+                                             (zipWith (\ a b -> a++"_"++b) (map (fill'left 3 '0'.show) [1..]) (map mkname $ extract_lesson_name tags))
+                                     (a_class_href "cl-lesson__lesson" tags))
 
 main = do
-  mapM (getEnv . ("pp101_"++)) ["LOGIN", "PASS"] >>= login
+  login'
   download_dest <- (\l -> if length l > 0 then head l else "downloads") <$> getArgs
   let dlf = (>> return []) . uncurry (goo download_dest) 
   
-  let commonfs = [\ (a,b) -> return $ [(head a, mkurl $ ("lesson-library"</>) b)] ] :: [T -> T']
-      -- fs :: [URLString -> IO [FilePath]]
-      fs :: [[T -> T']]
+  let commonfs = [\ (a,b) -> return $ [("", mkurl $ ("lesson-library"</>) b)] ]
       fs = [
-        [
-          \(a,b) -> b `extracturl` (\tags -> let hr = (a_class_href "ll-recommended-collection__study" tags)
-                                             in zip
-                                                (map basename hr)
-                                                hr),
-          \(a,b) -> b `extracturl` (\tags -> zip
-                                             (zipWith (\ a b -> a++"_"++b) (map (fill'left 3 '0'.show) [1..]) (map mkname $ extract_lesson_name tags))
-                                     (a_class_href "cl-lesson__lesson" tags)),
-          dlf
+        [a_href_basename "ll-collection-all"],
+        [a_href_basename "ll-recommended-collection__study"]
         ]
-        ]
-  -- f (map (applyToFst ((:[]) . show)) $ enumerate l) $ commonfs ++ fs
-  mapM_ (f (zip (repeat [""]) l) . (commonfs++)) $ fs
-
-  -- let go l = do
-  
-  --       putStrLn $ nice'title l
-  --       let url_section = mkurl $ "lesson-library/" </> l
-  --       url_recommended <- extracturl url_section (a_class_href "ll-recommended-collection__study")
-  --       -- case url_recommended of
-  --       --   Nothing -> putStrLn $ "there is nothing to download from " ++ url_section
-  --       --   Just u ->
         
-  --       (`mapM_` url_recommended) $ \u -> do
-  --           urls <- extracturl u (\tags -> zip (extract_lesson_name tags) (a_class_href "cl-lesson__lesson" tags))
-  --           print $ u ++ " ; " ++ show (length urls) ++ " urls found."        
-  --           mapM_ (goo download_dest l) (zip [1..] urls)
-
-  -- mapM go l
+  mapM_ (f (zip (map (:[]) l) l) . (++ [prepare_lesson, dlf]) . (commonfs++)) $ fs
   putStrLn "ok"
